9790d88280d822b6f29f591e88167a22220ccbc3
S-Rush <swrush98@yahoo.com>
2015-11-07 16:47:20 -0600

    Update upload.py
    File now receives a form with both a Place ID and an image
    Function declarations given for function to validate Place ID and function to add PlaceID, Image Filename pair to database

--- upload.py
+++ upload.py
@@ -1,10 +1,3 @@
-#
-#  NOTE: A NUMBER OF EDITS STILL NEED TO BE MADE FOR THIS 
-#  allowed_file must be made to allow files as we want
-#  file saving must be altered to save images as not onle .jpeg files
-#  ALLOWED_EXTENSIONS should have all the extensions we are allowing
-#  If there is a better way to generate file names, use it
-#
 import math
 import time
 from flask import Flask, request, redirect, url_for
@@ -12,28 +5,39 @@ app = Flask(__name__)
 ALLOWED_EXTENSIONS = set(['jpeg'])
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-fcount = 0 # This counts the number of files (for making file name) from curre$
+fcount = 0 # This counts the number of files (for making file name) from current one
 curtime = time.time() # Current time
 def allowed_file(filename):
-        return '.' in filename and \
-                filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
-
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+def checkpid(placeid): # Returns false if placeid invalid, otherwise true
+	return placeid
+def addphoto(placeid, filename): # Takes placeid of a photo and filename and adds photo to database matching placeid
+	return
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-        global curtime
-        global fcount
-        if request.method == 'POST':
-                if (int(curtime) != int(time.time())): # Restarting on file na$
-                        curtime = time.time()
-                        fcount = 0
-                fcount += 1
-                fname = str(int(curtime))+str(fcount)
-                file = request.files['dog']
-                if allowed_file(file.filename):
-                    file.save('static/uploads/'+fname+'.jpeg')
-                    return redirect('static/uploads/'+fname+'.jpeg')
-        return
+	global curtime
+	global fcount
+	if request.method == 'POST':
+		if (int(curtime) != int(time.time())): # Restarting on file names
+			curtime = time.time()
+			fcount = 0
+		fcount += 1
+		file = request.files['dog']
+		placeid = request.form['placeid']
+		if allowed_file(file.filename) and checkpid(placeid):
+	                fname = str(int(curtime))+str(fcount)+'.'+file.filename.rsplit('.', 1)[1]
+			file.save('static/uploads/'+fname)
+			addphoto(placeid, fname)
+			return redirect('static/uploads/'+fname)
+		elif checkpid(placeid):
+			return "Error: Forbidden filename"
+		else:
+			return "Error: Invalid place ID"
+	return
 
 if __name__ == '__main__':
     app.run()
-
