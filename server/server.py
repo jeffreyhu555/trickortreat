4216e7ec603d038d5f49647263e4890d7f4bbe24
Louis Goessling <louis@goessling.com>
2015-11-07 13:26:42 -0600

    Setup database

--- server/server.py
+++ server/server.py
@@ -1,10 +1,12 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+ 'mysql://602p:'+open("dbpassword",'r').read()+'@602p.mysql.pythonanywhere-services.com/602p$trickortreat'
 
 @app.route('/api/test')
 def hello_world():
     return '{"connected":true}'
+
+if __name__=="__main__":
+	app.run()
