640c9f0db93e411185b7b04f66ee60bcf3a972d4
Louis Goessling <louis@goessling.com>
2015-11-07 16:53:56 -0600

    adasd

--- server/server.py
+++ server/server.py
@@ -20,20 +20,20 @@ class House(db.Model):
 	candies = db.Column(db.String(5000))
 	avgcandy = db.Column(db.Float())
 	avgcandy_raters = db.Column(db.Integer())
-	photos = db.Column(db.String(5000)) # ### as seperator
+	photos = db.Column(db.Strinig(5000))# ### as seperator
 
-
-	def __init__(self, placeid, initial_note, initial_photo, intitial_rating, intitial_candy, initial_candies):
+	def __init__(self, placeid, initial_note, intitial_rating, intitial_candy, initial_candies):
 		self.placeid=placeid
 		self.address="<NOTCOMPUTED>"
 		self.lat=-1
 		self.lon=-1
 		self.notes=initial_note
-		self.rating=intitial_rating if intitial_rating else 0
-		self.rating_raters=1
-		self.avgcandy=intitial_candy
-		self.avgcandy_raters=1 if intitial_candy else 0
-		self.photos=initial_photo
+		self.rating=0 if initial_rating==None else intitial_rating
+		self.rating_raters=1 if intitial_rating!=None else 0
+		self.avgcandy=0 if initial_candy==None else intitial_candy
+		self.avgcandy_raters=1 if intitial_candy!=None else 0
+		self.candies=initial_candies
+		self.photos=""
 
 class Candy(db.Model):
 	id = db.Column(db.Integer(), primary_key=True)
@@ -44,26 +44,65 @@ class Candy(db.Model):
 		self.name=name
 		self.tags=tags
 
+lambda FAIL(code, data=None): json.dumps({"success":False, "error":code, "info":data})
+
+def reduce(l):
+	c=[]
+	for i in l:
+		if i not in c: c.append(i)
+	return c
+
 @app.route('/upload', methods=['POST', 'GET'])
 def api_upload():
+	if request.method!="POST": return FAIL("wrong_method")
+	if "?" not in request.url: return FAIL("no_placeid")
+	if not request.files or 'image' not in request.files: return FAIL("no_attachment")
+
+	placeid=request.url.split("?",1)[1]
+	f=request.files['image']
+	ext=f.filename.split(".",1)[1]
+
+	if ext not in ["png","jpg","jpeg","gif"]: return FAIL("invalid_extention")
+
+	record=db.session.query(House).filter_by(placeid=placeid)
+
+	if not record: return FAIL("no_such_placeid")
+
+	fname=placeid+str(len(record.images.split("###"))-1)+ext
+	record.images+=fname+"###"
+	db.session.commit()
+	f.save("static/uploads/"+fname)
+
+	return json.dumps({"success":True})
+
+@app.route('/submit', methods=['POST', 'GET'])
+def api_submit():
+	if request.method!="POST": return FAIL("wrong_method")
+	if not request.data: return FAIL("no_data")
 	try:
-		if request.method=="POST":
-			if "?" in request.url:
-				if request.files and 'image' in request.files:
-					imageid=request.url.split("?",1)[1]
-					f=request.files['image']
-					ext=f.filename.split(".",1)[1]
-					fname=imageid+"."+ext
-					if imageid and fname not in os.listdir("static/uploads/"):
-						if ext in ["png","jpg","jpeg","gif"]:
-							f.save("static/uploads/"+fname)
-							return json.dumps({"success":True})
-					return json.dumps({"success":False, "error":"invalid_extention"})
-				return json.dumps({"success":False, "error":"no_attachment"})
-			return json.dumps({"success":False, "error":"no_imageid"})
-		return json.dumps({"success":False, "error":"wrong_method"})
-	except BaseException as e:
-		return json.dumps({"success":False, "error":"internal_error", "python_error":str(e)})
+		data=json.loads(request.data)
+	except json.decoder.JSONDecodeError as e:
+		return FAIL("invalid_data", str(e))
+
+	if "placeid" not in data: FAIL("no_placeid")
+	if data["placeid"]=="": FAIL("invalid_placeid")
+
+	record=session.query(House).filter_by(placeid=data["placeid"])
+
+	if not record:
+		record=House(data["placeid"], data.get("note",""), data.get("rating"), data.get("candy"), "###".join(data.get("candies"))+("" if "candies" in data else "###"))
+		db.session.add(record)
+	else:
+		if "note" in data:
+			record.notes+=data["note"].replace("###","##")+"###"
+		if "rating" in data:
+			record.rating+=data["rating"] #TODO: Real calculation
+
+	db.session.commit()
+
+
+
+
 
 
 @app.route('/test')
